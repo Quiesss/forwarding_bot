ddd4360472ab6928a06b97382905480ee46eb637
Igor <igorshmal38@gmail.com>
2022-12-15 15:34:19 +0300

    fix forwarding

--- main.py
+++ main.py
@@ -1,44 +1,76 @@
 import asyncio
-import logging
+from typing import Any, Awaitable, Callable, Dict, List, Union
 
-from aiogram.filters import Command
-from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram import Router, Bot, Dispatcher
-from aiogram.types import Message
+from aiogram import BaseMiddleware, Bot, Dispatcher, F
+from aiogram.types import (
+    InputMediaAudio,
+    InputMediaDocument,
+    InputMediaPhoto,
+    InputMediaVideo,
+    Message,
+    TelegramObject,
+)
 
-API_TOKEN = '5889492973:AAHI1vdaIXv3H1lmPacgnHQThO49vHLBfDo'
+DEFAULT_DELAY = 0.6
 
-bot = Bot(API_TOKEN, parse_mode="HTML")
-dp = Dispatcher(storage=MemoryStorage())
-router = Router()
-
-
-@router.message(Command(commands=['start']))
-async def start_msg(message: Message):
-    await message.answer('Отправляйте сюда свое тз в строго следующем формате: \n\n'
-                         '<b>Гео: \n'
-                         'Название ПП и Email каба: \n'
-                         'Поток из ПП:\n'
-                         'Ваш порядковый номер: \n'
-                         'Комментарии: \n'
-                         'Домонетка: </b>\n')
+bot = Bot("5889492973:AAHI1vdaIXv3H1lmPacgnHQThO49vHLBfDo")
+dp = Dispatcher()
 
 
-@router.message()
-async def forward_message(message: Message):
-    await message.forward('-1001895213515')
-    await message.reply('Отправил, ожидайте')
+class MediaGroupMiddleware(BaseMiddleware):
+    ALBUM_DATA: Dict[str, List[Message]] = {}
+
+    def __init__(self, delay: Union[int, float] = DEFAULT_DELAY):
+        self.delay = delay
+
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: Message,
+        data: Dict[str, Any],
+    ) -> Any:
+        if not event.media_group_id:
+            return await handler(event, data)
+
+        try:
+            self.ALBUM_DATA[event.media_group_id].append(event)
+            return  # Don't propagate the event
+        except KeyError:
+            self.ALBUM_DATA[event.media_group_id] = [event]
+            await asyncio.sleep(self.delay)
+            data["album"] = self.ALBUM_DATA.pop(event.media_group_id)
+
+        return await handler(event, data)
 
 
-async def main():
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
-    )
-    dp.include_router(router)
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+@dp.message(F.media_group_id)
+async def handle_albums(message: Message, album: List[Message]):
+    """This handler will receive a complete album of any type."""
+    group_elements = []
+    for element in album:
+        print(element.caption)
+        caption_kwargs = {"caption": element.caption, "caption_entities": element.caption_entities}
+        if element.photo:
+            input_media = InputMediaPhoto(media=element.photo[-1].file_id, **caption_kwargs)
+        elif element.video:
+            input_media = InputMediaVideo(media=element.video.file_id, **caption_kwargs)
+        elif element.document:
+            input_media = InputMediaDocument(media=element.document.file_id, **caption_kwargs)
+        elif element.audio:
+            input_media = InputMediaAudio(media=element.audio.file_id, **caption_kwargs)
+        else:
+            return message.answer("This media type isn't supported!")
+
+        group_elements.append(input_media)
+    username = message.from_user.username if message.from_user.username else 'unknown'
+    await bot.send_message('-1001609075511', text=f'Сообщение от @{username}')
+    return await bot.send_media_group('-1001609075511', media=group_elements)
 
 
-if __name__ == '__main__':
-    asyncio.run(main())
+@dp.message()
+async def handle_albums(message: Message):
+    return await message.forward('-1001609075511')
+
+if __name__ == "__main__":
+    dp.message.middleware(MediaGroupMiddleware())
+    dp.run_polling(bot, allowed_updates=dp.resolve_used_update_types())
