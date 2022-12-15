ed02b7dcab1e17bc6a3254bd4c0e8a24302c3652
Igor <igorshmal38@gmail.com>
2022-12-15 16:51:52 +0300

    fix some

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 from typing import Any, Awaitable, Callable, Dict, List, Union
 
 from aiogram import BaseMiddleware, Bot, Dispatcher, F
+from aiogram.filters import Command
 from aiogram.types import (
     InputMediaAudio,
     InputMediaDocument,
@@ -12,8 +13,9 @@ from aiogram.types import (
 )
 
 DEFAULT_DELAY = 0.6
+CHAT_ID_TO_SEND = '-1001895213515'
 
-bot = Bot("5889492973:AAHI1vdaIXv3H1lmPacgnHQThO49vHLBfDo")
+bot = Bot("5919702782:AAFlUB_2dR4wRF-PfCoYLzSvKxnwYqBdWpE", parse_mode="HTML")
 dp = Dispatcher()
 
 
@@ -24,10 +26,10 @@ class MediaGroupMiddleware(BaseMiddleware):
         self.delay = delay
 
     async def __call__(
-        self,
-        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
-        event: Message,
-        data: Dict[str, Any],
+            self,
+            handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+            event: Message,
+            data: Dict[str, Any],
     ) -> Any:
         if not event.media_group_id:
             return await handler(event, data)
@@ -41,6 +43,17 @@ class MediaGroupMiddleware(BaseMiddleware):
             data["album"] = self.ALBUM_DATA.pop(event.media_group_id)
 
         return await handler(event, data)
+
+
+@dp.message(Command(commands=['start']))
+async def cmd_start(message: Message):
+    await message.answer('Отправляйте сюда свое тз в строго следующем формате: \n\n'
+                         '<b>Гео: \n'
+                         'Название ПП и Email каба: \n'
+                         'Поток из ПП: \n'
+                         'Ваш порядковый номер: \n'
+                         'Комментарии: \n'
+                         'Домонетка:</b>')
 
 
 @dp.message(F.media_group_id)
@@ -63,14 +76,18 @@ async def handle_albums(message: Message, album: List[Message]):
 
         group_elements.append(input_media)
     username = message.from_user.username if message.from_user.username else 'unknown'
-    await bot.send_message('-1001895213515', text=f'Сообщение от @{username}')
-    return await bot.send_media_group('-1001895213515', media=group_elements)
+    await message.reply('Отправил, ожидайте')
+    await bot.send_message(CHAT_ID_TO_SEND, text=f'Сообщение от @{username}')
+    return await bot.send_media_group(CHAT_ID_TO_SEND, media=group_elements)
 
 
 @dp.message()
 async def handle_albums(message: Message):
-    return await message.forward('-1001895213515')
+    await message.reply('Отправил, ожидайте')
+    return await message.forward(CHAT_ID_TO_SEND)
+
 
 if __name__ == "__main__":
     dp.message.middleware(MediaGroupMiddleware())
+    dp.include_router(router)
     dp.run_polling(bot, allowed_updates=dp.resolve_used_update_types())
