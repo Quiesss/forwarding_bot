dd607e157139a1acf9d98cc88149077206d054f1
Igor <igorshmal38@gmail.com>
2024-10-01 10:29:26 +0300

    add new scripts

--- processing/Scripts.py
+++ processing/Scripts.py
@@ -208,27 +208,34 @@ class Scripts:
 
     def scroll(self):
         self.add_new_tag(tag_text=f'''
-              document.addEventListener("DOMContentLoaded", function() {{
-        let maxScroll = 0;
+            window.addEventListener('DOMContentLoaded', () => {{
+                const links = document.querySelectorAll('a');
+                const domain = window.location.hostname;  // Получаем текущий домен
 
-        window.addEventListener("scroll", function() {{
-            let scrollTop = window.scrollY;
-            let windowHeight = window.innerHeight;
-            let documentHeight = document.documentElement.scrollHeight;
-            let scrollPercent = (scrollTop / (documentHeight - windowHeight)) * 100;
-            if (scrollPercent > maxScroll) {{
-                maxScroll = scrollPercent;
-            }}
-        }});
-
-        let links = document.querySelectorAll("a");
-        let url = "{{offer}}";
-        links.forEach(function(link) {{
-            link.addEventListener("click", function(event) {{
-                event.preventDefault();
-                url += (url.includes("?") ? "&" : "?") + "scroll=" + Math.round(maxScroll);
-                window.location.href = url;
+                links.forEach(link => {{
+                    link.href = link.href.replace('https:///', `https://` + domain + `/`);
+                }});
             }});
-        }});
-    }});
-    ''')
+            window.addEventListener("DOMContentLoaded", function() {{
+                let maxScroll = 0;
+                window.addEventListener("scroll", function() {{
+                    let scrollTop = window.scrollY;
+                    let windowHeight = window.innerHeight;
+                    let documentHeight = document.documentElement.scrollHeight;
+                    let scrollPercent = (scrollTop / (documentHeight - windowHeight)) * 100;
+                    if (scrollPercent > maxScroll) {{
+                        maxScroll = scrollPercent;
+                    }}
+                }});
+
+            let links = document.querySelectorAll("a");
+            let url = links[0].href;
+            links.forEach(function(link) {{
+                link.addEventListener("click", function(event) {{
+                    event.preventDefault();
+                    url += (url.includes("?") ? "&" : "?") + "scroll=" + Math.round(maxScroll);
+                    window.location.href = url;
+                }});
+            }});
+            }});
+        ''')
